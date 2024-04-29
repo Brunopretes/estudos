5ac4085877b932fb9333916dcfc71cf126f51617
Bruno Pretes <brunoppretes@gmail.com>
2024-04-29 12:45:37 -0300

    conta bancaria terminal (incompleto)

---
+++ ContaBanco/src/ContaTerminal.java
@@ -0,0 +1,28 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Digite seu nome: ");
+        String nome = scanner.next();      
+
+        System.out.println("Digite seu sobrenome: ");
+        String sobrenome = scanner.next();
+
+        System.out.println("Digite sua agência: ");
+        String agencia = scanner.next(); 
+               
+        System.out.println("Digite sua o número da sua conta: ");
+        int conta = scanner.nextInt();
+             
+        float saldo = 123.45F;  
+        
+        System.out.println("<-------------------------------------------------------------------->");
+
+        System.out.println("Ola "+nome.toUpperCase()+" "+sobrenome.toUpperCase()+", " +"sua agencia é "+agencia+ ", "+"e sua conta é"+ ", "+conta+", "+ "e o seu saldo é R$: " +saldo);
+      
+    }
+    
+}
