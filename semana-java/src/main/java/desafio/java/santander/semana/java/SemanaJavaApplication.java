bd3aff4d14b42783d430a19d37c4c96d55209b29
Bruno Pretes <brunoppretes@gmail.com>
2024-06-16 07:07:48 -0300

    continuação

---
+++ semana-java/src/main/java/desafio/java/santander/semana/java/SemanaJavaApplication.java
@@ -0,0 +1,13 @@
+package desafio.java.santander.semana.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemanaJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SemanaJavaApplication.class, args);
+	}
+
+}
---
+++ semana-java/src/test/java/desafio/java/santander/semana/java/SemanaJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package desafio.java.santander.semana.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SemanaJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
